8b007f2729a79ffb6391b3102f10957d08c86dd0
Tilly Davies <tillyd72@gmail.com>
2021-02-20 12:14:03 +0000

    Libraries and firebase

--- app/src/main/java/com/tilly/securenotes/MainActivity.kt
+++ app/src/main/java/com/tilly/securenotes/MainActivity.kt
@@ -2,10 +2,19 @@ package com.tilly.securenotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tilly.securenotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // Lateinit is a variable that cant be null but is initialized later
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
     }
 }
